4841a2fd7206c4c68f0034187ce713562eedec58
Chaitu <chaitanya.varma@digicollect.com>
2016-06-14 21:36:02 +0530

    changed settings.py

--- urlhacx/settings.py
+++ urlhacx/settings.py
@@ -70,8 +70,6 @@ TEMPLATES = [
     },
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-
 WSGI_APPLICATION = 'urlhacx.wsgi.application'
 
 
@@ -119,9 +117,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
