d9216fbb6a3db616cceb07953430a34a7501f8f3
chaitanya varma <chaitanya.varma@digicollect.com>
2016-06-15 18:09:42 +0530

    changed acces token

--- issues/views.py
+++ issues/views.py
@@ -9,7 +9,7 @@ from django.utils.lorem_ipsum import paragraph
 
 
 def getallissues(request):
-    val = requests.get('https://api.github.com/search/issues?q=state=closed&per_page=100&sort=created&dir=asc', headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
+    val = requests.get('https://api.github.com/search/issues?q=state=closed&per_page=100&sort=created&dir=asc', headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
     print val
     print dir(val)
     print type(val)
@@ -29,8 +29,8 @@ def get_issues_in_last_24hrs(request,page_id):
         print "hello"
         return render(request, 'getallissues.html', {'data': "" ,"page_id":"", "invalid":True})
     
-#     val = requests.get('https://api.github.com/search/issues?q=state:closed&since=2015-12-30T22:52:57Z&sort=created&per_page=100&order=desc&page=1', headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
-    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc&page='+str(page_id), headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
+#     val = requests.get('https://api.github.com/search/issues?q=state:closed&since=2015-12-30T22:52:57Z&sort=created&per_page=100&order=desc&page=1', headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
+    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc&page='+str(page_id), headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
     print val
     json_data = json.loads(val.text);
     print json_data["total_count"]
@@ -40,12 +40,12 @@ def get_issues_in_last_24hrs(request,page_id):
 def home(request):
     yesterday = datetime.now() - timedelta(days=1)
     print yesterday.isoformat()
-    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc', headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
+    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc', headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
     json_data = json.loads(val.text);
     week = datetime.now() - timedelta(days=7)
-    val1 = requests.get('https://api.github.com/search/issues?q=state:open+created:"'+week.isoformat()+' .. '+yesterday.isoformat()+'"&per_page=100&order=asc', headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
+    val1 = requests.get('https://api.github.com/search/issues?q=state:open+created:"'+week.isoformat()+' .. '+yesterday.isoformat()+'"&per_page=100&order=asc', headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
     week_data = json.loads(val1.text);
     yesterday = datetime.now() - timedelta(days=1)
-    val2 = requests.get('https://api.github.com/search/issues?q=state:open+created:<='+week.isoformat()+'&per_page=100&order=asc', headers={'Authorization': 'token 00e113440ea6faf74f2e0f88736588239b0ce2bd'})
+    val2 = requests.get('https://api.github.com/search/issues?q=state:open+created:<='+week.isoformat()+'&per_page=100&order=asc', headers={'Authorization': 'token 572774dcaa65348d51542a31e984c315dd94d753'})
     more_than_week_data = json.loads(val2.text);
     return render(request, 'home.html',{"yesterday_count":json_data["total_count"],"btw_yesterday_and_week":week_data["total_count"],"more_than_week_data":more_than_week_data["total_count"]})
