da7490ae66768823cb1d14b68f5d85fbd90bbc03
Chaitu <chaitanya.varma@digicollect.com>
2016-06-17 00:06:20 +0530

    added comments

--- issues/views.py
+++ issues/views.py
@@ -9,7 +9,7 @@ from django.utils.lorem_ipsum import paragraph
 
 
 def getallissues(request):
-    val = requests.get('https://api.github.com/search/issues?q=state=closed&per_page=100&sort=created&dir=asc')
+    val = requests.get('https://api.github.com/search/issues?q=state=open&per_page=100&sort=created&dir=asc')
     print val
     print dir(val)
     print type(val)
@@ -20,33 +20,28 @@ def getallissues(request):
 
 def get_issues_in_last_24hrs(request,page_id):
     yesterday = datetime.now() - timedelta(days=1)
-    print yesterday.isoformat()
-    print int(page_id)
     page_id = int(page_id)
-    print page_id>10
     
     if page_id>10 or page_id<1:
-        print "hello"
         return render(request, 'getallissues.html', {'data': "" ,"page_id":"", "invalid":True})
+        val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc&page='+str(page_id))
     
-#     val = requests.get('https://api.github.com/search/issues?q=state:closed&since=2015-12-30T22:52:57Z&sort=created&per_page=100&order=desc&page=1', headers={'Authorization': 'token 1525b4b004c6368157886f1e2e56f117b566a80f'})
-    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc&page='+str(page_id))
-    print val
     json_data = json.loads(val.text);
-    print json_data["total_count"]
-    print len(json_data["items"])
     return render(request, 'getallissues.html', {'data': json_data["items"] ,"page_id":page_id,"count":json_data["total_count"],"invalid":False})
 
 def home(request):
+#     get all open issues
+    all_issues = requests.get('https://api.github.com/search/issues?q=state=open&per_page=100&sort=created&dir=asc')
+    all_issues_json = json.loads(all_issues.text)
+#     get open issues in last 24 hours
     yesterday = datetime.now() - timedelta(days=1)
-    print yesterday.isoformat()
-    val = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc')
-    json_data = json.loads(val.text);
-    print json_data
+    yesterday_response = requests.get('https://api.github.com/search/issues?q=state:open+created:>='+yesterday.isoformat()+'&per_page=100&order=asc')
+    json_data = json.loads(yesterday_data.text);
+#     Number of open issues that were opened more than 7 days ago
     week = datetime.now() - timedelta(days=7)
-    val1 = requests.get('https://api.github.com/search/issues?q=state:open+created:"'+week.isoformat()+' .. '+yesterday.isoformat()+'"&per_page=100&order=asc')
-    week_data = json.loads(val1.text);
-    yesterday = datetime.now() - timedelta(days=1)
-    val2 = requests.get('https://api.github.com/search/issues?q=state:open+created:<='+week.isoformat()+'&per_page=100&order=asc')
-    more_than_week_data = json.loads(val2.text);
-    return render(request, 'home.html',{"yesterday_count":json_data["total_count"],"btw_yesterday_and_week":week_data["total_count"],"more_than_week_data":more_than_week_data["total_count"]})
+    week_response = requests.get('https://api.github.com/search/issues?q=state:open+created:"'+week.isoformat()+' .. '+yesterday.isoformat()+'"&per_page=100&order=asc')
+    week_data = json.loads(week_response.text);
+#   Number of open issues that were opened more than 24 hours ago but less than 7 days ago 
+    inbetween_response = requests.get('https://api.github.com/search/issues?q=state:open+created:<='+week.isoformat()+'&per_page=100&order=asc')
+    more_than_week_data = json.loads(inbetween_response.text);
+    return render(request, 'home.html',{"yesterday_count":json_data["total_count"],"btw_yesterday_and_week":week_data["total_count"],"more_than_week_data":more_than_week_data["total_count"],"all_issues":all_issues_json["total_count"]})
